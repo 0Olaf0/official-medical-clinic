25b5e1eda6b032c54b282750ba2c9e52514236ed
0Olaf0 <olafmazurkiewicz5@gmail.com>
2023-09-06 18:55:44 +0300

    budowa projektu

---
+++ src/main/java/com/github/Olaf0/officialmedicalclinic/OfficialMedicalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.github.Olaf0.officialmedicalclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialMedicalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialMedicalClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Olaf0/officialmedicalclinic/OfficialMedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Olaf0.officialmedicalclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialMedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
